d7b10f808918734912e61dd14a90a90511005566
Wes Alcock <bluebery@gmail.com>
2014-09-27 10:56:05 -0700

    Added nav bar and main navigational pages
    including bootstrap and jquery

---
+++ public/javascripts/main.js
@@ -0,0 +1,9 @@
+﻿/*
+    $(function () {
+        $('.navbar-nav li').click(function () {
+            $(this).siblings().removeClass('active');
+            $(this).addClass('active');
+        });
+});
+*/
+
---
+++ routes/movie.js
@@ -0,0 +1,12 @@
+﻿
+/*
+ * GET movie pages.
+ */
+
+exports.modifymovielanding = function (req, res) {
+	res.render('modifymovielanding', { title: 'Modify Movie' });
+};
+
+exports.removemovie = function (req, res) {
+	res.render('removemovie', { title: 'Remove Movie' });
+};
